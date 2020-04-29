a110dfd273e494059d6c0b26216b7b30b203c162
Nicole <chelseanbr@gmail.com>
2020-04-29 16:50:38 -0700

    finished mushroom classification

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+from graphviz import Source
+from sklearn.tree import export_graphviz
 
 
 # Pandas df exploration functions
@@ -37,12 +42,42 @@ def pipeline(df):
 
 # Plotting functions
 
-def plot_pie(series):
-    fig, ax = plt.subplots(figsize=(8,8))
+def plot_pie(series, fig, ax):
+    # fig, ax = plt.subplots(figsize=(8,8))
 
     series.value_counts().plot.pie(ax=ax, autopct='%1.2f%%')
 
     plt.rcParams['font.size'] = 18
-    plt.setp(series.index, fontsize=15)
+    
     fig.tight_layout()
     return fig, ax
+
+def plot_counts_bygroup(df, features, groupby, fig, axs):
+    # fig, axs = plt.subplots(6, 4, figsize=(14,18))
+
+    for feature, ax in zip(features, axs.flatten()[:len(features)]):
+        ax = sns.countplot(data=df, x=feature, hue=groupby, ax=ax)
+        ax.legend_.remove()
+
+    fig.tight_layout()
+    return fig, axs
+
+def plot_topN_features(feature_importances, feature_list, N):
+    # Plot the feature importance
+    idxes = np.argsort(-feature_importances)
+    feature_list[idxes]
+    rev_sort_feature_importances = feature_importances[idxes]
+    rev_sort_feature_cols = feature_list[idxes]
+
+    feat_scores = pd.DataFrame({'Fraction of Samples Affected' : rev_sort_feature_importances[:N]},
+                               index=rev_sort_feature_cols[:N])
+    feat_scores = feat_scores.sort_values(by='Fraction of Samples Affected')
+    feat_scores.plot(kind='barh')
+    
+    plt.title('Feature Importances', size=25)
+    plt.ylabel('Features', size=25)
+    return plt
+
+def plot_tree(tree, feature_list, out_file=None):
+    # Source(plot_tree(tree, feature_list, out_file=None)) to print in Jupyter nb
+    return export_graphviz(tree, out_file=out_file, feature_names=feature_list)
